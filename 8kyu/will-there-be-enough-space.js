d2b2df08fb8e046156b8b5324083eaedf800301f
dom <35171942+domluther@users.noreply.github.com>
2024-04-21 17:32:15 +0100

    daily codewars

--- 8kyu/will-there-be-enough-space.js
+++ 8kyu/will-there-be-enough-space.js
@@ -14,3 +14,6 @@
 // cap = 100, on = 60, wait = 50 --> 10 # He can't fit 10 of the 50 waiting
 
 // My solution
+function enough(cap, on, wait) {
+  return on + wait <= cap ? 0 : Math.abs(cap - (wait + on));
+}
