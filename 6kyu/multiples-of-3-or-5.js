bf6ac981de318ee663222683c038ab29a50451c2
dom <35171942+domluther@users.noreply.github.com>
2024-05-21 19:01:24 +0100

    daily codewars

--- 6kyu/multiples-of-3-or-5.js
+++ 6kyu/multiples-of-3-or-5.js
@@ -25,3 +25,15 @@ function solution(number) {
   }
   return total;
 }
+
+// Not using a ternary
+function solution(number) {
+  var sum = 0;
+
+  for (var i = 1; i < number; i++) {
+    if (i % 3 == 0 || i % 5 == 0) {
+      sum += i;
+    }
+  }
+  return sum;
+}
