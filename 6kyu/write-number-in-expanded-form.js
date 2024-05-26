746528689f21332a0a67f3c6b5b5ea9cb5d301ac
dom <35171942+domluther@users.noreply.github.com>
2024-05-26 08:50:42 +0100

    daily codewars

--- 6kyu/write-number-in-expanded-form.js
+++ 6kyu/write-number-in-expanded-form.js
@@ -18,3 +18,14 @@ function expandedForm(num) {
 }
 
 console.log(expandedForm(1023));
+
+// With reverses
+const expandedForm = (n) =>
+  n
+    .toString()
+    .split('')
+    .reverse()
+    .map((a, i) => a * Math.pow(10, i))
+    .filter((a) => a > 0)
+    .reverse()
+    .join(' + ');
