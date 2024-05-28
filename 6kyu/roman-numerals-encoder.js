f5dfe3d3edd548554a8573d60db4fb8439668abb
dom <35171942+domluther@users.noreply.github.com>
2024-05-28 17:12:14 +0100

    daily codewars

--- 6kyu/roman-numerals-encoder.js
+++ 6kyu/roman-numerals-encoder.js
@@ -52,3 +52,36 @@ function generateText(value, base, mid, next) {
 
 ans = solution(2024);
 console.log(ans);
+
+// Alternate idea
+// Take away values, each time you take that many years off, concatenate the appropriate character
+function solution(number) {
+  // convert the number to a roman numeral
+  var roman = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+
+  var ans = '';
+  while (number > 0) {
+    for (a in roman) {
+      if (roman[a] <= number) {
+        ans += a;
+        number -= roman[a];
+        break;
+      }
+    }
+  }
+  return ans;
+}
