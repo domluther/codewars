a0f31c85a566a9b61d2b15cea79d0946edc7d494
dom <35171942+domluther@users.noreply.github.com>
2024-05-29 07:58:33 +0100

    daily codewars

--- 7kyu/count-the-digit.js
+++ 7kyu/count-the-digit.js
@@ -16,3 +16,36 @@
 // 1, 16, 81, 100, 121, 144, 169, 196, 361, 441.
 // So there are 11 digits 1 for the squares of numbers between 0 and 25.
 // Note that 121 has twice the digit 1.
+
+function nbDig(n, d) {
+  // Create all the squares in range 0 to n (+1 on n as zero indexed). Create as strings because will be used as them later.
+  const squares = Array.from({ length: n + 1 }, (num, index) =>
+    String(Math.pow(index, 2))
+  );
+  console.log(squares);
+  //   Iterate through the numbers, how many times does the digit d appear - use split to take this into account and count the number of parts
+  //   1 taken away as eg  splitting 100 by 1 gives you 1 and 00
+  let count = 0;
+  return squares.reduce((acc, cur) => acc + cur.split(d).length - 1, 0);
+
+  //    for (let i = 0; i < squares.length; i++) {
+  //     count += squares[i]
+  //       .split('')
+  //       .reduce((acc, cur) => (cur == d ? acc + 1 : acc), 0);
+  //   }
+  //   return count;
+}
+
+// Shorter - concatenate all the digits
+function concatDigit(n, d) {
+  let all = '';
+  for (let i = 0; i <= n; i++) {
+    all += Math.pow(i, 2);
+  }
+  return all.split(d).length - 1;
+}
+
+// const ans = nbDig(5750, 0);
+// console.log(ans);
+let ans2 = concatDigit(25, 1);
+console.log(ans2);
