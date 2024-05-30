c8dc36bef4393c9c3d042895619aeade0bb63eec
dom <35171942+domluther@users.noreply.github.com>
2024-05-30 08:26:51 +0100

    daily codewars

--- 7kyu/find-the-capitals.js
+++ 7kyu/find-the-capitals.js
@@ -6,7 +6,17 @@
 var capitals = function (word) {
   // Iterate through word
   // Check char - if it is uppercase then add to array
+  const output = [];
+  word
+    .split('')
+    .forEach((char, index) =>
+      char.toUpperCase() === char ? output.push(index) : ''
+    );
+
+  return output;
 };
 
 const ans = capitals('CodEWaRs');
 console.log(ans);
+
+// Note - wouldn't work if there were numbers - would need to eg check if charCode in range 65 - 90
