7d0390bb94c198d3bbc861f470ce4ade83428d17
dom <35171942+domluther@users.noreply.github.com>
2024-05-30 08:21:56 +0100

    daily codewars

---
+++ 7kyu/find-the-capitals.js
@@ -0,0 +1,12 @@
+// Write a function that takes a single non-empty string of only lowercase and uppercase ascii letters (word) as its argument, and returns an ordered list containing the indices of all capital (uppercase) letters in the string.
+
+// Example (Input --> Output)
+// "CodEWaRs" --> [0,3,4,6]
+
+var capitals = function (word) {
+  // Iterate through word
+  // Check char - if it is uppercase then add to array
+};
+
+const ans = capitals('CodEWaRs');
+console.log(ans);
