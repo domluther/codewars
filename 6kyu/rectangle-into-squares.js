cdb587c4f53890e5aa59df7384d6dd7dac0fdc82
dom <35171942+domluther@users.noreply.github.com>
2024-05-31 07:57:35 +0100

    daily codewars

--- 6kyu/rectangle-into-squares.js
+++ 6kyu/rectangle-into-squares.js
@@ -13,8 +13,24 @@
 //   sqInRect(3, 5) should return [3, 2, 1, 1]
 
 function sqInRect(length, width) {
+  // Already a square? Return null
   if (length === width) return null;
+  const sizes = [];
+  // Split up into squares by saying which is the shortest side, making a square of that and then seeing what is left. This is repeated while there are squares left.
+  while (length > 0 && width > 0) {
+    if (width < length) {
+      sizes.push(width);
+      length -= width;
+    } else if (length < width) {
+      sizes.push(length);
+      width -= length;
+    } else {
+      sizes.push(length);
+      length -= length;
+    }
+  }
+  return sizes;
 }
 
-const ans = sqInRect(5, 5);
+const ans = sqInRect(8, 5);
 console.log(ans);
