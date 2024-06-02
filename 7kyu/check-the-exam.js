141373cff2a567c7c51c67333702ea8cb7be0d01
dom <35171942+domluther@users.noreply.github.com>
2024-06-02 09:33:32 +0100

    daily codewars

--- 7kyu/check-the-exam.js
+++ 7kyu/check-the-exam.js
@@ -41,3 +41,20 @@ function checkExam(array1, array2) {
 
 const ans = checkExam(['b', 'c', 'b', 'a'], ['', 'a', 'a', 'c']);
 console.log(ans);
+
+// Using reduce
+function checkExam(array1, array2) {
+  const reducer = (a, e, idx) => {
+    if (e === '') {
+      return a;
+    }
+
+    if (e === array1[idx]) {
+      return (a += 4);
+    }
+
+    return --a;
+  };
+  const score = array2.reduce(reducer, 0);
+  return score < 0 ? 0 : score;
+}
