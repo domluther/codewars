ff5f138f1c9e4a37a4cac740da114c1476f8909b
dom <35171942+domluther@users.noreply.github.com>
2024-06-02 20:58:02 +0100

    daily codewars

---
+++ 6kyu/multiplication-table.js
@@ -0,0 +1,15 @@
+// DESCRIPTION:
+// Your task, is to create N×N multiplication table, of size provided in parameter.
+
+// For example, when given size is 3:
+
+// 1 2 3
+// 2 4 6
+// 3 6 9
+
+multiplicationTable = function (size) {
+  // insert code here
+};
+
+const ans = multiplicationTable(3);
+console.log(ans);
