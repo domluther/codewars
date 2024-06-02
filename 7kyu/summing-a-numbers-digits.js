01aafbae95135efa519fa0e6d2505429298d25bc
dom <35171942+domluther@users.noreply.github.com>
2024-06-02 09:44:44 +0100

    daily codewars

--- 7kyu/summing-a-numbers-digits.js
+++ 7kyu/summing-a-numbers-digits.js
@@ -8,9 +8,26 @@
 // Let's assume that all numbers in the input will be integer values.
 
 function sumDigits(number) {
-  let sum = 0;
+  // Absolute to deal with possible - signs, could also have just replaced it out but abs seemed faster
+  let sum = String(Math.abs(number))
+    .split('')
+    .reduce((acc, cur) => Number(acc) + Number(cur));
   return sum;
 }
 
 const ans = sumDigits(92);
 console.log(ans);
+
+// Not using reduce
+function sumDigits(number) {
+  // Turn number into absolute value -> Stringify number -> Split at each character
+  var values = String(Math.abs(number)).split('');
+  // Instantiate variable to hold math result
+  var product = null;
+  // For each value, add the value to the product
+  for (var i = 0; i < values.length; i++) {
+    product = product + Number(values[i]);
+  }
+  // Return the product variable
+  return product;
+}
