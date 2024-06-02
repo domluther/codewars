e4616bf7e63356c13a0003d75e39a99d20c550ec
dom <35171942+domluther@users.noreply.github.com>
2024-06-02 09:32:40 +0100

    daily codewars

--- 7kyu/check-the-exam.js
+++ 7kyu/check-the-exam.js
@@ -12,3 +12,32 @@
 //  ["a", "a", "c", "b"]   ["a", "a", "b", "" ]  →     7
 //  ["a", "a", "b", "c"]   ["a", "a", "b", "c"]  →     16
 //  ["b", "c", "b", "a"]   ["" , "a", "a", "c"]  →     0
+
+function checkExam(array1, array2) {
+  // Iterate through the two arrays
+  // blank? 0
+  // match? +4
+  // different -1
+  let score = 0;
+
+  for (let i = 0; i < array1.length; i++) {
+    console.log(`Comparing ${array1[i]} to ${array2[i]}`);
+    console.log(array1[i] == array2[i]);
+    if (array2[i] === '') {
+      console.log('blank');
+      score += 0;
+    } else if (array1[i] === array2[i]) {
+      console.log('Match');
+      score += 4;
+    } else {
+      console.log('Different');
+      score -= 1;
+    }
+  }
+
+  //   If the score is less than 0, return 0
+  return score < 0 ? 0 : score;
+}
+
+const ans = checkExam(['b', 'c', 'b', 'a'], ['', 'a', 'a', 'c']);
+console.log(ans);
