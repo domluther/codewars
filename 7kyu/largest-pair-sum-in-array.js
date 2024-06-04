966b02af5cc3eecc2b322d09834f71548c43e4be
dom <35171942+domluther@users.noreply.github.com>
2024-06-04 18:11:31 +0100

    daily codewars

---
+++ 7kyu/largest-pair-sum-in-array.js
@@ -0,0 +1,18 @@
+// DESCRIPTION:
+// Given a sequence of numbers, find the largest pair sum in the sequence.
+
+// For example
+
+// [10, 14, 2, 23, 19] -->  42 (= 23 + 19)
+// [99, 2, 2, 23, 19]  --> 122 (= 99 + 23)
+// Input sequence contains minimum two elements and every element is an integer.
+
+function largestPairSum(numbers) {
+  let sum = 0;
+  //TODO: Write your Code here
+  return 0;
+}
+
+let vals = [10, 14, 2, 23, 19];
+const ans = largestPairSum(vals);
+console.log(ans);
