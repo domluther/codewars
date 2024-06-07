f9eea9dcce39884372a7615ee657360f85a92b21
dom <35171942+domluther@users.noreply.github.com>
2024-06-07 10:21:57 +0100

    daily codewars

--- 6kyu/allocating-hotel-rooms.js
+++ 6kyu/allocating-hotel-rooms.js
@@ -14,7 +14,70 @@
 
 // NOTE: The list of customers is not necessarily ordered by arrival_time.
 
+// Let me suggest one approach. Go through the customers in order by arrival day. For each customer, go through the already allocated rooms. If that customer's arrival day is larger than the largest departure day of any customer who has been previously allocated that room, the customer can go into that room. If no such room exists, create a new room and put the customer there.
+
 function allocateRooms(customers) {
+  // Have a list of rooms
+  const rooms = Array(0).fill(0);
+  // An array to store the bookings
+
+  const bookings = Array(customers.length).fill(0);
+  let booked = 0;
+  const bookedIds = [];
+  // While I've not booked everyone
+  while (booked < customers.length) {
+    let earliestDetails = findEarliestArrival(customers, bookedIds);
+    bookRoom(rooms, bookings, earliestDetails);
+    booked++;
+  }
+
+  return bookings;
+}
+
+function findEarliestArrival(customers, bookedIds) {
+  let earliestArrival = Infinity;
+  let earliestId;
+  let departureDay;
+  // Iterate through the customers to find who arrives first
+  for (let id = 0; id < customers.length; id++) {
+    const [arrival, departure] = customers[id];
+    // Find the earliest arrival that hasn't already been dealt with
+    if (arrival < earliestArrival && !bookedIds.includes(id)) {
+      earliestArrival = arrival;
+      departureDay = departure;
+      earliestId = id;
+    }
+  }
+  // Keep track of which ids have been booked
+  bookedIds.push(earliestId);
+  return { earliestArrival, earliestId, departureDay };
+}
+
+function bookRoom(rooms, bookings, earliestDetails) {
+  let arrival = earliestDetails.earliestArrival;
+  let departure = earliestDetails.departureDay;
+  let found = false;
+  let roomNum = 1;
+  // Not found a room and there's a room to look at
+  while (!found && roomNum <= rooms.length) {
+    // If that room is available, book it
+    if (rooms[roomNum - 1] < arrival) {
+      rooms[roomNum - 1] = departure;
+      found = true;
+      // This is where the problem was - I misunderstood that the array was meant to be ordered by customers not arrival time
+      bookings[earliestDetails.earliestId] = roomNum;
+    }
+    // That room wasn't valid? Check the next
+    roomNum++;
+  }
+  // Wasn't space? Add a new room and book it
+  if (!found) {
+    rooms.push(departure);
+    bookings[earliestDetails.earliestId] = roomNum;
+  }
+}
+
+function allocateRoomsOriginal(customers) {
   // Have a list of rooms
   // Short cut to start - create a list of 50 rooms that are all 0.
   const rooms = Array(1000).fill(0);
@@ -45,16 +108,11 @@ function allocateRooms(customers) {
 // Works but not efficient if out of order
 
 const input = [
-  [8, 8],
-  [5, 8],
-  [8, 9],
-  [1, 4],
-  [1, 3],
-  [5, 7],
-  [4, 8],
-  [2, 2],
-  [4, 5],
-  [6, 8],
+  [15, 22],
+  [2, 4],
+  [6, 9],
+  [3, 33],
+  [12, 21],
 ];
 const ans = allocateRooms(input);
 console.log(ans);
