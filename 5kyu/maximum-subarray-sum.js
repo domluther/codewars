989109601de28b26f45f4ebfc08b6a4b2b85f749
dom <35171942+domluther@users.noreply.github.com>
2024-06-09 13:26:34 +0100

    daily codewars

--- 5kyu/maximum-subarray-sum.js
+++ 5kyu/maximum-subarray-sum.js
@@ -7,9 +7,37 @@
 
 // Empty list is considered to have zero greatest sum. Note that the empty list or array is also a valid sublist/subarray.
 
+// Easy leetcode - solved O(n^2)
 var maxSequence = function (arr) {
+  // Empty list 0 sum
+  if (arr.length === 0) return 0;
   // ...
+  let max = 0;
+  let current;
+  for (let start = 0; start < arr.length; start++) {
+    current = 0;
+    for (let i = start; i < arr.length; i++) {
+      current += arr[i];
+      if (current > max) max = current;
+      console.log(current);
+    }
+  }
+  return max;
 };
 
-const ans = maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]);
+function maxSequenceSliding(arr) {
+  if (arr.length === 0) return 0;
+  // First value is the max
+  let max = arr[0];
+  let current = 0;
+  for (let i = 0; i < arr.length; i++) {
+    // Try adding the current with the next item, if it's not bigger then the window restarts
+    current = current + arr[i] > arr[i] ? current + arr[i] : arr[i];
+    max = Math.max(current, max);
+  }
+  return max;
+}
+
+const sequence = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+const ans = maxSequenceSliding(sequence);
 console.log(ans);
