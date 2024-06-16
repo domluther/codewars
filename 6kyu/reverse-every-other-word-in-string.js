45e40df2683fcda2804e38c81510ba72de7b31a7
dom <35171942+domluther@users.noreply.github.com>
2024-06-16 16:20:13 +0100

    codewars time

---
+++ 6kyu/reverse-every-other-word-in-string.js
@@ -0,0 +1,13 @@
+// Reverse every other word in a given string, then return the string. Throw away any leading or trailing whitespace, while ensuring there is exactly one space between each word. Punctuation marks should be treated as if they are a part of the word in this kata.
+
+function reverse(str) {
+  //WRITE SOME MAGIC
+}
+
+const test1 = 'Reverse this string, please!';
+const test2 = "I really don't like reversing strings!";
+
+const ans1 = reverse(test1);
+console.log(ans1);
+const ans2 = reverse(test2);
+console.log(ans2);
