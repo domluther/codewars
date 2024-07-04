0cdb49983e650b77ad4d9169449e989131d2da15
dom <35171942+domluther@users.noreply.github.com>
2024-07-04 22:49:30 +0100

    codewars - texting

--- 6kyu/multitap-keypad-text-entry-on-an-old-mobile-phone.js
+++ 6kyu/multitap-keypad-text-entry-on-an-old-mobile-phone.js
@@ -17,11 +17,54 @@
 // Tested phrases contain letters (A-Z and a-z), digits (0-9), and special characters # and *.
 
 function presses(phrase) {
-  // To do...
+  const keysNeeded = {
+    0: 2,
+    1: 1,
+    2: 4,
+    3: 4,
+    4: 4,
+    5: 4,
+    6: 4,
+    7: 5,
+    8: 4,
+    9: 5,
+    '*': 1,
+    '#': 1,
+    ' ': 1,
+    A: 1,
+    B: 2,
+    C: 3,
+    D: 1,
+    E: 2,
+    F: 3,
+    G: 1,
+    H: 2,
+    I: 3,
+    J: 1,
+    K: 2,
+    L: 3,
+    M: 1,
+    N: 2,
+    O: 3,
+    P: 1,
+    Q: 2,
+    R: 3,
+    S: 4,
+    T: 1,
+    U: 2,
+    V: 3,
+    W: 1,
+    X: 2,
+    Y: 3,
+    Z: 4,
+  };
+  return phrase
+    .split('')
+    .reduce((acc, cur) => acc + keysNeeded[cur.toUpperCase()], 0);
 }
 
 const inp1 = 'LOL';
 const inp2 = 'HOW R U';
-const ans = presses('LOL');
+const ans = presses(inp2);
 // 9
 console.log(ans);
