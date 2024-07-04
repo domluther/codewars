2e1ca78609b0ce00c4f170f955c7f378b08ff46e
dom <35171942+domluther@users.noreply.github.com>
2024-07-04 21:15:20 +0100

    codewars - a11y

--- 6kyu/word-a10n-abbreviation.js
+++ 6kyu/word-a10n-abbreviation.js
@@ -6,9 +6,45 @@
 // The abbreviated version of the word should have the first letter, then the number of removed characters, then the last letter (eg. "elephant ride" => "e6t r2e").
 
 function abbreviate(string) {
-  // ...
+  // Split into parts
+  const words = string.split(' ');
+  const output = [];
+  // Variable that holds what is going to be added
+  let toPush;
+  // Loop through the words
+  for (word of words) {
+    toPush = '';
+    if (word.length < 4) {
+      toPush = word;
+    } else if (word.includes('-')) {
+      const hyphenated = word.split('-');
+      const hyphenatedOutput = [];
+      let hyphenPush;
+      for (hyphenatedWord of hyphenated) {
+        if (hyphenatedWord.length < 4) {
+          hyphenPush = hyphenatedWord;
+        } else {
+          hyphenPush = shortenWord(hyphenatedWord);
+        }
+        hyphenatedOutput.push(hyphenPush);
+        console.log(hyphenatedOutput);
+      }
+      toPush = hyphenatedOutput.join('-');
+    } else {
+      toPush = shortenWord(word);
+    }
+    output.push(toPush);
+  }
+  return output.join(' ');
 }
 
-const inp1 = 'internationalization';
+function shortenWord(word) {
+  // Words with a comma need shortening without thinking about the comma, then add it back
+  if (word.endsWith(',')) return `${word[0]}${word.length - 3}${word.at(-2)},`;
+
+  return `${word[0]}${word.length - 2}${word.at(-1)}`;
+}
+
+const inp1 = 'You need, need not want, to complete this code-wars mission';
 const ans = abbreviate(inp1);
 console.log(ans);
